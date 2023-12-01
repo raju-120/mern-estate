4a7e8cf816e39d628bd1da8978483331e321fea4
Raju <ashfaqraju27@gmail.com>
2023-12-01 12:06:35 +0600

    google sign up working

--- client/src/Pages/Profile/Profile.jsx
+++ client/src/Pages/Profile/Profile.jsx
@@ -1,12 +1,19 @@
 import {useSelector} from 'react-redux';
+import { useRef } from 'react';
 
 export default function Profile() {
+
   const {currentUser} = useSelector((state) =>state.user);
+  const fileRef = useRef(null);
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
       <form className='flex flex-col gap-4'> 
-        <img src={currentUser.avatar} 
+        
+        <input type="file" ref={fileRef} hidden accept='image/*'/>
+
+        <img onClick={() =>fileRef.current.click()} src={currentUser.avatar} 
             className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2' 
             alt="profile" 
         />
