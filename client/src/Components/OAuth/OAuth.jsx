caf9d6f7f6859979bf425981d447574b7f89b04a
Raju <ashfaqraju27@gmail.com>
2023-12-02 17:10:16 +0600

    complete image upload functionality on profile to the firebase storage

--- client/src/Components/OAuth/OAuth.jsx
+++ client/src/Components/OAuth/OAuth.jsx
@@ -42,5 +42,6 @@ export default function OAuth() {
     <button onClick={handleGoogleClick} type="button" 
     className="bg-red-700 text-white p-3 rounded-lg uppercase
      hover:opacity-95"> Continue with Google</button>
-  )
+  );
 }
+
