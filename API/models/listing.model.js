779526a459ad1b8d665bef090ed8c4a385548575
Raju <ashfaqraju27@gmail.com>
2023-12-11 01:10:01 +0600

    complete image upload functionality

--- API/models/listing.model.js
+++ API/models/listing.model.js
@@ -6,6 +6,10 @@ const listingSchema = new mongoose.Schema(
             type: String,
             required: true,
         },
+        description: {
+            type: String,
+            required: true,
+          },
         address: {
             type: String,
             required: true,
